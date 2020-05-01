7cfb506b327b05c0b1ebeade257e87c645299a3d
shahadmalenezi <shahadmalenezi@gmail.com>
2020-05-01 16:53:16 -0700

    Initial Commit

---
+++ shahad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shahad
+//
+//  Created by Meshal Alenezi on 5/1/20.
+//  Copyright © 2020 shahadmalenezi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
